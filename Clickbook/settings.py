69575fd59c0eeefe884e41475d5e10430108f197
arendonc2 <124947616+arendonc2@users.noreply.github.com>
2025-05-21 09:16:54 -0500

    Update settings.py

--- Clickbook/settings.py
+++ Clickbook/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-z68(&qv$i@kj+ejrb&2fl8_6o*o2v_n=16l6efyjoe8=z5qwd(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.88.192.152']
+ALLOWED_HOSTS = ['54.166.166.179/']
 
 
 # Application definition
